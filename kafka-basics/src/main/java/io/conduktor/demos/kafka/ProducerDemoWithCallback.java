330ecab91d3a9d143e2460f3b48abf149c58b8bb
William Chong <william@inorbit.com>
2023-02-20 07:53:16 +0400

    feat: sticky partitioner in batch

--- kafka-basics/src/main/java/io/conduktor/demos/kafka/ProducerDemoWithCallback.java
+++ kafka-basics/src/main/java/io/conduktor/demos/kafka/ProducerDemoWithCallback.java
@@ -2,17 +2,17 @@ package io.conduktor.demos.kafka;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RoundRobinPartitioner;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Properties;
 
-public class ProducerDemo {
-
+public class ProducerDemoWithCallback {
     private static final Logger log = LoggerFactory.getLogger(ProducerDemo.class.getSimpleName());
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         log.info("I am a Kafka Producer!");
 
         // Create producer properties
@@ -28,18 +28,41 @@ public class ProducerDemo {
         properties.setProperty("sasl.mechanism", "PLAIN");
 
         // Set the producer properties
-        properties.setProperty("key.serializer", StringSerializer.class.getName());
+        properties.setProperty("key.serializer", StringSerializer .class.getName());
         properties.setProperty("value.serializer", StringSerializer.class.getName());
+
+//        properties.setProperty("batch.size", "400");
+
+//        properties.setProperty("partitioner.class", RoundRobinPartitioner.class.getName());
 
         // Create the producer
         KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
 
-        // Create producer record
-        ProducerRecord<String, String> record =
-                new ProducerRecord<>("demo_java", "hello world!");
+        for (int j = 0; j < 10; j++) {
+            for (int i = 0; i < 30; i++) {
+                // Create producer record
+                ProducerRecord<String, String> record =
+                        new ProducerRecord<>("demo_java", "hello world " + i);
 
-        // Send data
-        producer.send(record);
+                // Send data
+                producer.send(record, (metadata, exception) -> {
+                    // executes every time a record is successfully sent or an exception is thrown
+                    if (exception == null) {
+                        // the record was successfully sent
+                        log.info("Received new metadata. \n" +
+                                "Topic: " + metadata.topic() + "\n" +
+                                "Partition: " + metadata.partition() + "\n" +
+                                "Offset: " + metadata.offset() + "\n" +
+                                "Timestamp: " + metadata.timestamp());
+                    } else {
+                        log.error("Error while producing", exception);
+                    }
+                });
+            }
+
+            Thread.sleep(500);
+        }
+
 
         // tell the producer to send all data and block until done --synchronous
         producer.flush();
